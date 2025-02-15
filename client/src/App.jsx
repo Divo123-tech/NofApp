812f6c6567244cacd2f9b410d5778fb12784423c
Divo123 <topherharsh@gmail.com>
2025-02-16 00:47:29 +0800

    added footer

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,20 +1,28 @@
-import HomePage from './components/HomePage'
-import SignIn from './components/SignIn';
-import Register from './components/Register';
-import './App.css'
-import { Route, Routes, useLocation} from "react-router-dom";
+import HomePage from "./components/HomePage";
+import SignIn from "./components/SignIn";
+import Register from "./components/Register";
+import Footer from "./components/Footer";
+import "./App.css";
+import { Route, Routes, useLocation } from "react-router-dom";
 function App() {
   const location = useLocation();
+  const hideFooter =
+    location.pathname == "/" ||
+    location.pathname == "/register" ||
+    location.pathname == "/sign-in";
   return (
-    <div className="bg-[#d2f8bd] h-screen">
-      <Routes location={location}>
-      <Route path={"/"} element={<HomePage />} />
-      <Route path={"/sign-in"} element={<SignIn />} />
-      <Route path={"/register"} element={<Register />} />
-        
-      </Routes>
+    <div className="flex flex-col min-h-screen bg-[#d2f8bd]">
+      <div className="flex-grow">
+        <Routes location={location}>
+          <Route path={"/"} element={<HomePage />} />
+          <Route path={"/sign-in"} element={<SignIn />} />
+          <Route path={"/register"} element={<Register />} />
+        </Routes>
+      </div>
+
+      {!hideFooter && <Footer />}
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
