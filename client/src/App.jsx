d39637e5aa845bdf00dee3f91066bc810f8c0cc1
Divo123 <topherharsh@gmail.com>
2025-02-16 03:35:48 +0800

    added top footer

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -5,8 +5,8 @@ import Tracker from "./components/Tracker";
 import Footer from "./components/Footer";
 import "./App.css";
 import { Route, Routes, useLocation } from "react-router-dom";
-import Charity from "./components/Charity"
-import './App.css'
+import Charity from "./components/Charity";
+import "./App.css";
 function App() {
   const location = useLocation();
   const hideFooter =
@@ -15,7 +15,14 @@ function App() {
     location.pathname == "/sign-in";
   return (
     <div className="flex flex-col h-screen bg-[#d2f8bd]">
-      <div className="flex-grow">
+      {/* Top Section (Like a "Top Footer") */}
+      {!hideFooter && <div className="h-20 flex flex-col items-center font-semibold italic font-[Karla] py-4">
+        <h1 className="text-2xl mr-8 text-[#2183d2]">NoF</h1>
+        <h1 className="text-2xl ml-8 text-[#2183d2]">App</h1>
+      </div>}
+
+      {/* Main Content (Takes up remaining space) */}
+      <div className="flex-grow flex flex-col">
         <Routes location={location}>
           <Route path={"/"} element={<HomePage />} />
           <Route path={"/sign-in"} element={<SignIn />} />
@@ -23,11 +30,11 @@ function App() {
           <Route path={"/tracker"} element={<Tracker />} />
           <Route path={"/charities"} element={<Charity />} />
         </Routes>
+      </div>
+
+      {/* Footer (Always at the bottom) */}
       {!hideFooter && <Footer />}
-      </div>
-      </div>
-
-
+    </div>
   );
 }
 
