da26c316971e47a912be7477243e735c21d87bed
Ellen Faustine <ellenfaustine831@gmail.com>
2025-02-16 02:21:39 +0800

    added tracker

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,18 +1,12 @@
-<<<<<<< HEAD
 import HomePage from "./components/HomePage";
 import SignIn from "./components/SignIn";
 import Register from "./components/Register";
+import Tracker from "./components/Tracker";
 import Footer from "./components/Footer";
 import "./App.css";
 import { Route, Routes, useLocation } from "react-router-dom";
-=======
-import HomePage from './components/HomePage'
-import SignIn from './components/SignIn';
-import Register from './components/Register';
 import Tracker from './components/Tracker';
 import './App.css'
-import { Route, Routes, useLocation} from "react-router-dom";
->>>>>>> 9be889c (stash)
 function App() {
   const location = useLocation();
   const hideFooter =
@@ -20,28 +14,19 @@ function App() {
     location.pathname == "/register" ||
     location.pathname == "/sign-in";
   return (
-<<<<<<< HEAD
     <div className="flex flex-col h-screen bg-[#d2f8bd]">
       <div className="flex-grow">
         <Routes location={location}>
           <Route path={"/"} element={<HomePage />} />
           <Route path={"/sign-in"} element={<SignIn />} />
           <Route path={"/register"} element={<Register />} />
+          <Route path={"/tracker"} element={<Tracker />} />
         </Routes>
+      {!hideFooter && <Footer />}
+      </div>
       </div>
 
-      {!hideFooter && <Footer />}
-=======
-    <div className="bg-[#d2f8bd] h-screen">
-      <Routes location={location}>
-      <Route path={"/"} element={<HomePage />} />
-      <Route path={"/sign-in"} element={<SignIn />} />
-      <Route path={"/register"} element={<Register />} />
-      <Route path={"/tracker"} element={<Tracker />} />
-        
-      </Routes>
->>>>>>> 9be889c (stash)
-    </div>
+
   );
 }
 
