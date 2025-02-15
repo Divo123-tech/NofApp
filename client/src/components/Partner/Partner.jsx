0b80b7786979b2a831a21342df84e136481ee94b
Ellen Faustine <ellenfaustine831@gmail.com>
2025-02-16 04:02:19 +0800

    stash

--- client/src/components/Partner/Partner.jsx
+++ client/src/components/Partner/Partner.jsx
@@ -6,8 +6,8 @@ const Partner = () => {
     <div className="min-h-screen bg-[#d2f8bd] flex flex-col items-center justify-start px-6 py-8">
 
       <div className="w-full text-left ml-10 mb-8">
-        <h1 className="text-2xl md:text-3xl font-bold text-[#2183d2] mb-3">Username</h1>
-        <button class="bg-[#2183d2] text-white font-bold py-2 px-4 rounded-full">Remove accountability partner</button> 
+        <h1 className="text-3xl md:text-3xl font-bold text-[#2183d2] mb-3">Marcus Tng</h1>
+        <button class="bg-red-600 text-white font-semibold py-2 px-4 rounded-full">Remove accountability partner</button> 
       </div>
       <div className="w-full text-left ml-10">
         <h1 className="text-md md:text-3xl text-[#2183d2] mb-3">My accountability partner's calendar</h1>
